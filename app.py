f7637d117da239bf71aefec0f3a040740a5bc344
Qingyao Sun <nalzok@berkeley.edu>
2018-11-03 20:52:05 -0700

    Removed sensitive data

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
+import os
 import requests
 from flask import Flask
 from flask_restful import Resource, Api
 
-
-key = '10586160-ad0baa48d53693e1114bf544d'
+key = os.environ['PIXABAY_API_KEY']
 
 app = Flask(__name__)
 api = Api(app)
@@ -14,14 +14,22 @@ def summary_text(text):
     return keyword
 
 
+class Hello(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
 class MakeSuggestion(Resource):
     def get(self, text):
         payload = {'key': key, 'q': summary_text(text)}
         r = requests.get('https://pixabay.com/api/', params=payload)
+        if r.status_code != 200:
+            return 'there is some error'
         resp = r.json()
         return [hit['largeImageURL'] for hit in resp['hits']]
 
 
+api.add_resource(Hello, '/')
 api.add_resource(MakeSuggestion, '/api/<string:text>')
 
 if __name__ == '__main__':
